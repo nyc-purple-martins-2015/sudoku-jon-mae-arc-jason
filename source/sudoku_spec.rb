ccc513ad8b319acdb92327e2a774d2c9b39805ea
Mae Capozzi <email": null>
2015-10-02 14:40:24 -0400

    Add outline of Sudoku Class
    Now we have full outline of Sudoku Class

---
+++ source/sudoku_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'sudoku'
+
+describe 'Sudoku' do
+  it 'expects Sudoku to be a class' do
+    expect(Sudoku).to be_a(Class)
+  end
+end
+
+end
+
+# solve a row
+# solve a column
+# solve solve a box
